b4df8c5b8cd3623428446d84d1d39a2b89659e36
Alex Harris <72664735+HarrisAlex@users.noreply.github.com>
2024-03-08 16:27:21 -0500

    Added javascript template

---
+++ main.js
@@ -0,0 +1,41 @@
+// Import functions from Firebase CDN
+
+// Firebase Configuration
+
+// Initialize Firebase
+
+
+// Get all required references to HTML elements
+
+
+// Get username from the API local storage
+
+// Check if username exists
+
+// Send message to the database
+function sendMessage(event) {
+
+}
+
+// Create a message element
+function createMessageElement(message, sender) {
+   
+}
+
+// Listen for new messages
+
+
+// Handle login
+function onLogin(event) {
+
+}
+
+// Handle logout
+function onLogout() {
+
+}
+
+// Set the username and refresh page
+function setUsername(username) {
+
+}
